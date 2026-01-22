cda6e756730b46d0a012b258c40de3d6c7b51459
Miguel Moreira <87150446+MountainClimber0@users.noreply.github.com>
2026-01-22 15:57:51 -0400

    adicionando variáveis dinâmicas
    entendendo a logica de ponteiros e trocando variáveis estáticas por dinâmicas

--- batalha naval/batalha naval.c
+++ batalha naval/batalha naval.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include<time.h>
+#include<stdlib.h>
 #define TAM_STRING 50
 #define MAX_PAISES 5
-#include <string.h>
-#include "uteis.c"
 
+#include "uteis.c"
+//definicao da struct territorio
  typedef struct Territorio
 {
     /* list of country names; initialize instances separately */
@@ -17,8 +20,16 @@
 int main() {
 
  
-struct Territorio territorio[MAX_PAISES];
+Territorio *territorio = malloc( MAX_PAISES * sizeof(Territorio));
 
+if (territorio == NULL) {
+    printf("Erro ao alocar memoria\n");
+    return 1;
+}
+
+
+ 
+//contador para auxiliar na atribuicao de valores
 int numPaises = 0;
 
 printf("Bem vindo ao jogo de batalha naval!\n");
@@ -26,20 +37,34 @@ printf("Para comecar insira o numero de paises:\n");
 scanf("%d", &numPaises);
 limparBuffer();
 
-for (int i = 0; i < numPaises ; i++) {
-    printf("Insira o nome do pais %d: \n", i+1);
-    scanf("%s", territorio[i].paises);
 
+//loop para atribuir valores as structs
+for (int i = 0; i < numPaises ; i++) {
+
+    printf("Insira o nome do pais %d: \n", i+1);
+        scanf("%s", territorio[i].paises);
+
+    printf("Insira a cor do pais %d: \n", i+1);
+        scanf("%s", territorio[i].corTerritorio);
+
+    printf("Insira o numero de tropas do pais %d: \n", i+1);
+        scanf("%d", &territorio[i].numTropas);
+
+     limparBuffer();
+     
 }
 
 printf("seus paises sao:\n");
-
+printf("====================:\n");
+//loop para mostrar os valores atribuídos
 for(int i = 0; i < numPaises ; i++){
 
-  printf("%d - %s\n", i + 1, territorio[i].paises);
-
-
+printf("%s\n", territorio[i].paises);
+printf("cor : %s\n", territorio[i].corTerritorio);
+printf("com %d tropas\n", territorio[i].numTropas);
+printf("====================\n");
 }
+free(territorio);
 
 return 0;
-};
+}; 
