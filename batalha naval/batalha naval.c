922eb8385ada704e38ba92c20ed93fd0bbe6b182
Miguel Moreira <87150446+MountainClimber0@users.noreply.github.com>
2026-03-03 12:26:54 -0400

    finish-project
    i forgot to commit all the changes from past days but here is the "finished" project. It has some bugs and i didnt fix the errors when the user does not use the expected input.

--- batalha naval/batalha naval.c
+++ batalha naval/batalha naval.c
@@ -14,7 +14,129 @@
   char corTerritorio [TAM_STRING];
      int numTropas;
 
-} Territorio;
+} 
+Territorio;
+
+int numPaises = 0;
+
+
+//contador para auxiliar na atribuicao de valores
+  int getNumeroPaises(void){
+     
+scanf("%d", &numPaises);
+
+
+if (numPaises > MAX_PAISES){
+
+    printf("Numero maximo de paises excedido, o maximo e %d\n", MAX_PAISES);
+    return 1;
+}else if (numPaises <= 0){
+    printf("Numero de paises deve ser maior que 0\n");
+    return 1;
+
+}
+limparBuffer();
+return 0;
+
+
+
+}
+
+//loop para atribuir valores as structs
+void getPaisesData(Territorio *territorio, int numPaises){
+for (int i = 0; i < numPaises ; i++) {
+
+    printf("Insira o nome do pais %d: \n", i+1);
+        scanf("%s", territorio[i].paises);
+
+    printf("Insira a cor do pais %d: \n", i+1);
+        scanf("%s", territorio[i].corTerritorio);
+
+    printf("Insira o numero de tropas do pais %d: \n", i+1);
+        scanf("%d", &territorio[i].numTropas);
+
+     limparBuffer();
+    
+}
+}
+
+// mostrar dados dos paises para o ususario
+void displayPaisesData(Territorio *territorio, int numPaises){
+printf("Numero de paises: %d\n", numPaises);
+printf("seus paises sao:\n");
+printf("====================:\n");
+//loop para mostrar os valores atribuídos
+for(int i = 0; i < numPaises ; i++){
+    
+printf("%d. %s ( %s, tropas: %d )\n", i+1, territorio[i].paises, territorio[i].corTerritorio , territorio[i].numTropas);
+}
+
+}
+
+int turnoAtaque(Territorio *territorio, int numPaises){
+
+int paisAtacante, paisDefensor;
+
+
+    for(int i = 0; i < numPaises ; i++){
+int escolhaAtaque; 
+paisAtacante = territorio[i].numTropas;
+
+    
+    printf("turno do pais %d\n", i+1);
+printf("paises disponiveis para atacar:\n");
+
+for(int j = 0; j < numPaises ; j++){
+     if(j != i){
+        
+        printf("%d. %s\n", j+1, territorio[j].paises);
+printf("qual pais voce deseja atacar? (digite o numero correspondente)\n");
+
+     }
+}
+
+scanf("%d", &escolhaAtaque);
+
+if (escolhaAtaque < 1 || escolhaAtaque > numPaises || escolhaAtaque == i+1){
+    printf("Escolha invalida, tente novamente\n");
+    return 1;
+}
+
+int forcaAtaque = rand() % paisAtacante + 1;
+paisDefensor = territorio[escolhaAtaque - 1].numTropas;
+int forcaDefesa = rand() % paisDefensor + 1;
+
+   for(int k = 0; k < numPaises ; k++){
+    if(k == escolhaAtaque - 1){
+        if (forcaAtaque > forcaDefesa){
+            territorio[k].numTropas -= forcaDefesa;
+            printf("Ataque bem sucedido! O pais %s perdeu %d tropas\n", territorio[k].paises, forcaDefesa);
+            printf("O pais %s tem agora %d tropas\n", territorio[k].paises, territorio[k].numTropas);
+                limparBuffer();
+        } else {
+            territorio[i].numTropas -= forcaAtaque;
+            printf("Defesa bem sucedida! O pais %s perdeu %d tropas\n", territorio[i].paises, forcaAtaque);
+            printf("O pais %s tem agora %d tropas\n", territorio[i].paises, territorio[i].numTropas);
+            limparBuffer();
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+    }
+
+
+return 0;
+}
+
+
 
 
 int main() {
@@ -28,43 +150,20 @@ if (territorio == NULL) {
 }
 
 
- 
-//contador para auxiliar na atribuicao de valores
-int numPaises = 0;
-
 printf("Bem vindo ao jogo de batalha naval!\n");
 printf("Para comecar insira o numero de paises:\n");
-scanf("%d", &numPaises);
-limparBuffer();
+
+ getNumeroPaises();
+
+getPaisesData(territorio, numPaises);
+
+displayPaisesData(territorio, numPaises);
+
+turnoAtaque(territorio, numPaises);
 
 
-//loop para atribuir valores as structs
-for (int i = 0; i < numPaises ; i++) {
 
-    printf("Insira o nome do pais %d: \n", i+1);
-        scanf("%s", territorio[i].paises);
-
-    printf("Insira a cor do pais %d: \n", i+1);
-        scanf("%s", territorio[i].corTerritorio);
-
-    printf("Insira o numero de tropas do pais %d: \n", i+1);
-        scanf("%d", &territorio[i].numTropas);
-
-     limparBuffer();
-     
-}
-
-printf("seus paises sao:\n");
-printf("====================:\n");
-//loop para mostrar os valores atribuídos
-for(int i = 0; i < numPaises ; i++){
-
-printf("%s\n", territorio[i].paises);
-printf("cor : %s\n", territorio[i].corTerritorio);
-printf("com %d tropas\n", territorio[i].numTropas);
-printf("====================\n");
-}
 free(territorio);
-
 return 0;
-}; 
+}
+
